d886efe3cda001ecca0dc66b8c9a71f87624a71d
marax27 <kacpert314@gmail.com>
2020-04-04 10:17:17 +0200

    Clean up navigation bar. Configure app colors in vuetify.js

---
+++ vue-client/src/components/NavBar.vue
@@ -0,0 +1,26 @@
+<template>
+    <v-app-bar
+      app
+      color="primary"
+      dark
+    >
+      <div class="d-flex align-center">
+        <v-img
+          alt="Vuetify Logo"
+          class="shrink mr-2"
+          contain
+          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
+          transition="scale-transition"
+          width="40"
+        />
+
+        <v-toolbar-title>AI-Radiologist</v-toolbar-title>
+      </div>
+    </v-app-bar>
+</template>
+
+<script>
+export default {
+    name: 'NavBar'
+}
+</script>
--- vue-client/src/plugins/vuetify.js
+++ vue-client/src/plugins/vuetify.js
@@ -1,7 +1,17 @@
 import Vue from 'vue';
 import Vuetify from 'vuetify/lib';
+import colors from 'vuetify/lib/util/colors';
+
+const lightColorPalette = {
+    primary: colors.indigo.darken2
+};
 
 Vue.use(Vuetify);
 
 export default new Vuetify({
+    theme: {
+        themes: {
+            light: lightColorPalette
+        }
+    }
 });
