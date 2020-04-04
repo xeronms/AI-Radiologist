3619f8a9acdaaab73268160231d6bb53dba770bd
marax27 <kacpert314@gmail.com>
2020-04-04 13:02:33 +0200

    Extract pixel values from image

--- vue-client/src/components/AnalysisSummary.vue
+++ vue-client/src/components/AnalysisSummary.vue
@@ -14,6 +14,8 @@
       <p>File name: {{file.name}}</p>
       <p>File size: {{file.size}}B</p>
     </v-flex>
+
+    <img id="canvas-holder" style='position:absolute; visibility: hidden'/>
   </v-layout> 
 </template>
 
@@ -25,20 +27,48 @@ export default {
     },
     data: function() {
         return {
-            scoreMessage: '?'
+            scoreMessage: '?',
+            canvas: null,
+            ctx: null,
+            imageData: null
         };
     },
     mounted: function() {
-        const fileScore = this.processFile(this.file);
-        this.scoreMessage = `${Math.round(fileScore * 100)}`;
+        this.canvas = document.createElement('canvas');
+        this.ctx = this.canvas.getContext('2d');
+
+        this.assertFileReader();
+        this.processFile(this.file);
     },
     methods: {
+        computeScore: function() {
+            // Use imageData.
+            console.dir(this.imageData);
+
+            // Get and display score.
+            const score = Math.random();
+            this.scoreMessage = `${Math.round(score * 100)}`;
+        },
         imageUrl: function() {
             return URL.createObjectURL(this.file);
         },
         processFile: function(file) {
-            console.log(file);
-            return Math.random();
+            var fr = new FileReader();
+            fr.onload = () => this.showImage(fr);
+            fr.readAsDataURL(file);
+        },
+        showImage: function(fileReader) {
+            var img = document.getElementById('canvas-holder');
+            img.onload = () => this.getImageData(img);
+            img.src = fileReader.result;
+        },
+        getImageData: function(img) {
+            this.ctx.drawImage(img, 0, 0);
+            this.imageData = this.ctx.getImageData(0, 0, img.width, img.height).data;
+            this.computeScore();
+        },
+        assertFileReader: function() {
+            if (!FileReader) throw 'FileReader not supported';
         }
     }
 }
