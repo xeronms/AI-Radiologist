d9a54ca51bc9cea5b1a9ce50421b24b4161dafda
Mikołaj <xeronms@gmail.com>
2020-04-04 18:20:05 +0200

    tensorflow js integration

--- vue-client/src/components/AnalysisSummary.vue
+++ vue-client/src/components/AnalysisSummary.vue
@@ -17,11 +17,17 @@
       </v-btn>
     </v-flex>
 
+<<<<<<< Updated upstream
     <img id='canvas-holder'/>
+=======
+    <img id="canvas-holder" style='position:absolute; visibility: hidden'/>
+>>>>>>> Stashed changes
   </v-layout> 
 </template>
 
 <script>
+import * as tf from '@tensorflow/tfjs';
+
 export default {
     name: 'AnalysisSummary',
     props: {
@@ -44,14 +50,44 @@ export default {
         this.processFile(this.file);
     },
     methods: {
-        computeScore: function() {
+        computeScore: async function() {
             // Use imageData.
-            // console.dir(this.imageData);
+            console.dir(this.imageData);
+
+            // var rgb = [[]];
+            // for (var i=0; i < this.imageData.lenght; i+4){
+            //     rgb.push([this.imageData[i],
+            //                 this.imageData[i+1],
+            //                 this.imageData[i+2]]);
+            // }
+
+            const MODEL_PATH ='http://localhost:8080/model/model.json'
+
+            console.log('Loading model...');
+            let model = await tf.loadLayersModel(MODEL_PATH);
+
+            const imageTensor3d = tf.browser
+                                    .fromPixels(this.imageData, 3)
+                                    .resizeBilinear([224, 224])
+                                    .cast('float32')
+                                    .div(tf.scalar(255));
+            const imageTensor4d = tf.tensor4d(Array.from(imageTensor3d.dataSync()),[1,224,224,3]);
+
+            console.dir(imageTensor3d);
+            console.dir(imageTensor4d);
+
+            const prediction = model.predict(imageTensor4d, {batch_size: 1});
+            
+            console.log("prediction.print()");
+            prediction.print();
+            console.log("tostring: \n"+prediction.toString());
+            console.log("argMax():");
+            prediction.argMax().print();
 
             // Get and display score.
-            const score = Math.random();
+            const score = prediction.argMax().dataSync()[0];
+            console.log(score);
             this.scoreMessage = `${Math.round(score * 100)}`;
-            this.scoreDescription = this.getScoreDescription(score);
         },
         getScoreDescription(score) {
             if (score < 0.4)  return 'It probably isn\'t COVID-19-related.';
@@ -74,11 +110,18 @@ export default {
         },
         getImageData: function(img) {
             this.ctx.drawImage(img, 0, 0);
-            this.imageData = this.ctx.getImageData(0, 0, img.width, img.height).data;
+            this.imageData = this.ctx.getImageData(0, 0, img.width, img.height);
             this.computeScore();
         },
         assertFileReader: function() {
             if (!FileReader) throw 'FileReader not supported';
+        },
+        loadModel: async function() {
+             const MODEL_PATH ='http://localhost:8080/model/model.json'
+
+            console.log('Loading model...');
+            let model = await tf.loadLayersModel(MODEL_PATH);
+            return model
         }
     }
 }
