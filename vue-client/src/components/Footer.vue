e04600d76615d2db208e940f401a0eb326fa3672
Mikołaj <xeronms@gmail.com>
2020-04-05 05:25:44 +0200

    footer text correction

--- vue-client/src/components/Footer.vue
+++ vue-client/src/components/Footer.vue
@@ -3,13 +3,13 @@
     <v-card flat tile class="mx-auto secondary white--text text-center">
 
       <v-card-text class="white--text">
-        This application does not provide medical advice It is intended for informational purposes only. It is not a substitute for professional medical advice, diagnosis or treatment.
+        {{ new Date().getFullYear() }} — <strong>AI Radiologist</strong>
       </v-card-text>
 
       <v-divider></v-divider>
 
       <v-card-text class="white--text">
-        {{ new Date().getFullYear() }} — <strong>AI Radiologist</strong>
+        This application does not provide medical advice. It is not a substitute for professional diagnosis or treatment. It is intended for informative purposes only.
       </v-card-text>
     </v-card>
   </v-footer>
